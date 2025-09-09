b267185278e8584ccd767f56f610aad277f83427
Epoupa Nyadjroh <epoupaw@gmail.com>
2025-09-09 11:50:05 +0100

    Created a vue app using npm create vue@3, Setup the app, created a component characters

---
+++ peaky_blinder-forum/src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import Characters from './components/Characters.vue'
+export default { data(){
+        return{
+          showTitle: "Peaky Blinders",
+          synopsis: "A crime drama about a Birmingham family gang known as the Peaky Blinders. The show takes place in England from 1919 to 1930's before the start of the second world war ",
+          genre: ['crime', 'drama', 'action'],
+          numberOfSeasons: 6,
+          firstAired: 2014,
+          stillRunning: false,
+
+        }
+      },
+      components: {
+        Characters
+        }
+}
+</script>
+
+<template>
+  <div>
+      <h1>{{ showTitle }}</h1>
+      <p>Released: <i>{{firstAired}}</i>, Still Running: {{ stillRunning ? "Yes" : "No" }}, Number of Seasons: {{ numberOfSeasons }}</p>
+      <h2>Synopsis</h2>
+      <p>{{ synopsis }}</p>
+      <p><b>Genre: </b> <span v-for="(topic, index) in genre" :key="`genre-${index}`"> <i>{{ topic + " " }}</i> </span> </p>
+      <Characters />
+    </div>
+</template>
+
---
+++ peaky_blinder-forum/src/components/Characters.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  data(){
+    return {
+      characters: [{name: "Thomas Shelby", dob:"01-22-1979"},{name:"Aunt Poly", dob:"01-22-1966"},{name:"Arthur Shelby", dob:"01-22-1977"}],
+      favouriteList: [],
+      newCharacter: {name: undefined}
+    }
+  },
+    computed:{
+        numberOfCharacters(){
+          return this.characters.length
+        }
+      },
+      methods:{
+        addToFavouriteList(character){
+          if (this.favouriteList.includes(character)){
+            return
+          }
+          this.favouriteList.push(character)
+        },
+        addCharacter(){
+          this.characters.push(this.newCharacter)
+        }
+      }
+}
+</script>
+
+<template>
+  <h2>Characters ({{ numberOfCharacters }})</h2>
+      <input type="text" v-model="newCharacter.name" @keyup.enter="addCharacter()">
+      <ul v-if="characters.length > 0">
+        <li v-for="(character, index) in characters" :key="`character-${index}`">{{ character.name }} <button @click="addToFavouriteList(character)">Add to Favourites</button></li>
+      </ul>
+      <p v-else>No characters</p>
+      <h2>Favourite Characters</h2>
+      <p v-if="favouriteList.length === 0">No favourite Character</p>
+      <ul v-else>
+        <li v-for="(character, index) in favouriteList" :key="`favourite-${index}`">{{ character.name }}</li>
+      </ul>
+</template>
